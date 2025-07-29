9b66a6ffe5417432901faed2beb330fd1e1fdabd
sadik saroar <sadiksaroar275@gmail.com>
2025-07-29 06:00:55 +0600

    first commit

---
+++ lib/model/modelClass.dart
@@ -0,0 +1,24 @@
+class userClass {
+  int? userId;
+  int? id;
+  String? title;
+  String? body;
+
+  userClass({this.userId, this.id, this.title, this.body});
+
+  userClass.fromJson(Map<String, dynamic> json) {
+    userId = json['userId'];
+    id = json['id'];
+    title = json['title'];
+    body = json['body'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['userId'] = this.userId;
+    data['id'] = this.id;
+    data['title'] = this.title;
+    data['body'] = this.body;
+    return data;
+  }
+}
